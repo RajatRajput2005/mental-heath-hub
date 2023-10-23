bbb8f0cb280ac5c4514faf0fe7cd67799151f1c1
RajatRajput2005 <123498502+RajatRajput2005@users.noreply.github.com>
2023-10-23 12:21:38 +0530

    Add files via upload
    some files are added for more work

---
+++ SIH PROJECT/d.js
@@ -0,0 +1,13 @@
+// script.js
+
+function redirectToDashboard() {
+    window.location.href = 'dashboard.html';
+}
+
+function redirectToEmotionLog() {
+    // Add logic to redirect to the emotion log page
+}
+
+function redirectToProgressTracker() {
+    // Add logic to redirect to the progress tracker page
+}
---
+++ SIH PROJECT/login.js
@@ -0,0 +1,24 @@
+// main.js
+// script.js
+
+function login() {
+    var userType = document.getElementById('user_type').value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Perform login validation here (not implemented in this example)
+    // You can make an AJAX request to the server for validation
+    // For simplicity, we'll just show a message here
+    alert('Login function called');
+}
+
+function signup() {
+    var signupUserType = document.getElementById('signup_user_type').value;
+    var signupUsername = document.getElementById('signup_username').value;
+    var signupPassword = document.getElementById('signup_password').value;
+
+    // Perform signup validation here (not implemented in this example)
+    // You can make an AJAX request to the server for validation
+    // For simplicity, we'll just show a message here
+    alert('Signup function called');
+}
